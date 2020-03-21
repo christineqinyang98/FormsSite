930022b08b2797a7639e885b8444642f2996eb0e
christineqinyang98 <christineisonthejob@hotmail.com>
2020-03-20 23:57:32 -0400

    finished sample form

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // JavaScript source code
 const express = require('express')
 const app = express();
-
-app.get('/', (req, res) => {
+var root = '/Users/chris/source/repos/SampleForms'
+/*app.get('/', (req, res) => {
     res.send('Hello World!')
+});*/
+
+app.get('/', function (req, res) {
+    res.sendFile(root + '/views/Home.html');
 });
 
 app.listen(8000, () => {
